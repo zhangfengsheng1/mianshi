de263917706fd75349acbc7c42ede70c560b35a5
cangyezhi_admin <1019095884@qq.com>
2023-05-18 16:19:23 +0800

    完成

--- src/main.tsx
+++ src/main.tsx
@@ -4,18 +4,18 @@
  * @Author: CYZ
  * @Date: 2023-05-17 16:16:13
  * @LastEditors: CYZ
- * @LastEditTime: 2023-05-17 20:20:25
+ * @LastEditTime: 2023-05-18 16:17:19
  */
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
